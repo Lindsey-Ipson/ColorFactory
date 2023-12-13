35da7d938c04ec997af1c464e70ceda2c3359d95
Lindsey Ipson <lindseyipson@Lindseys-MacBook-Air.local>
2023-12-13 15:21:38 -0800

    add App component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <NavBar dogs={App.defaultProps.dogs} />
+      <Routes>
+        <Route path="/colors/:color" element={<Color />} />
+        <Route path="/colors" element={<ColorList />} />
+        <Route path="/colors/new" element={<ColorPicker />} />
+        <Route path="*" element={<Navigate to="/colors" />} />
+      </Routes>
+    </BrowserRouter>
+    // <div className="App">
+    //   <h1> Welcome to the Color Factory </h1>
+    //   <Link to={`/colors/new`}>Add new color</Link>
+    //   <ColorList />
+    // </div>
   );
 }
 
